fd4e9601bf9d39f99052b83a70101f1594224d3e
chicoumbetov <shyngysmaaan@gmail.com>
2025-06-18 22:27:21 +0200

    :sparkles: subscriptions module generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ScpiModule } from './scpi/scpi.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
 @Module({
-  imports: [UsersModule, ScpiModule],
+  imports: [UsersModule, ScpiModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
