5d7a931e1fde0e744569be05f58ab0a17a500901
chicoumbetov <shyngysmaaan@gmail.com>
2025-06-18 22:41:37 +0200

    :recycle: scpi entity

--- src/scpi/entities/scpi.entity.ts
+++ src/scpi/entities/scpi.entity.ts
@@ -1,1 +1,16 @@
-export class Scpi {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('scpi_units')
+export class ScpiUnit {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string; // e.g., "Iroko Zen"
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  pricePerUnit: number; // Current price of one SCPI unit
+
+  @Column({ default: true })
+  isActive: boolean;
+}
