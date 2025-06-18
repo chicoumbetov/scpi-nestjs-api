89da68b92397cb4320f90b8ab2be4db25cb59c46
chicoumbetov <shyngysmaaan@gmail.com>
2025-06-18 22:57:41 +0200

    :recycle: subscriptions module ts

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { SubscriptionsService } from './subscriptions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScpiUnitsModule } from 'src/scpi/scpi.module';
+import { UsersModule } from 'src/users/users.module';
+import { Subscription } from './entities/subscription.entity';
 import { SubscriptionsController } from './subscriptions.controller';
+import { SubscriptionsService } from './subscriptions.service';
 
 @Module({
-  controllers: [SubscriptionsController],
+  imports: [
+    TypeOrmModule.forFeature([Subscription]),
+    UsersModule, // to use UsersService
+    ScpiUnitsModule, // to use ScpiUnitsService
+  ],
   providers: [SubscriptionsService],
+  controllers: [SubscriptionsController],
 })
 export class SubscriptionsModule {}
