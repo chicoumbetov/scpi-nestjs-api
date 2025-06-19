c824b9ee5dac238d6a72cd55adfcc47ade91e154
chicoumbetov <shyngysmaaan@gmail.com>
2025-06-19 10:52:14 +0200

    :sparkles: main ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
@@ -9,6 +10,15 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('SCPI Nest JS Api')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('scpi')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(3006);
 }
 bootstrap();
